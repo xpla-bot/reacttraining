0f2164b534a8b99ab93ffedcc49ec76844bfdd8d
Stanislav Lesiuk <stanislav.lesiuk@intive.com>
2018-06-14 09:08:06 +0200

    Add some markup and styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    const titleText = 'Welcome to React';
+    const titleText = 'Awesome React Store';
     const paragraphText = 'Here is some text';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">{titleText}</h1>
+          <nav className="App-nav">
+            <li><a href="#">First</a></li>
+            <li><a href="#">Second</a></li>
+            <li><a href="#">Third</a></li>
+          </nav>
+          <div>
+            Cart total: $0
+          </div>
         </header>
+        <h1 className="App-title">{titleText}</h1>
         <p className="App-intro">{paragraphText}</p>
+        <section className="App-content">
+          <article className="App-product">
+            <h3>Item name</h3>
+            <p>
+              Curabitur ut tellus commodo, pretium enim quis, faucibus dui. Suspendisse consequat, leo a imperdiet
+              sollicitudin, odio purus faucibus tortor, id venenatis risus neque vel lacus. Pellentesque quis eros ac mi
+              auctor porta. Proin vitae sagittis diam. Nunc nec mi et ante feugiat consequat. Morbi sit amet dictum
+              augue, ac gravida justo. Morbi scelerisque consectetur lacus vitae fermentum.
+            </p>
+            <div>Price: $35</div>
+            <button type="button">Add to cart</button>
+          </article>
+          <article className="App-product">
+            <h3>Item name</h3>
+            <p>
+              Curabitur ut tellus commodo, pretium enim quis, faucibus dui. Suspendisse consequat, leo a imperdiet
+              sollicitudin, odio purus faucibus tortor, id venenatis risus neque vel lacus. Pellentesque quis eros ac mi
+              auctor porta. Proin vitae sagittis diam. Nunc nec mi et ante feugiat consequat. Morbi sit amet dictum
+              augue, ac gravida justo. Morbi scelerisque consectetur lacus vitae fermentum.
+            </p>
+            <div>Price: $35</div>
+            <button type="button">Add to cart</button>
+          </article>
+          <article className="App-product">
+            <h3>Item name</h3>
+            <p>
+              Curabitur ut tellus commodo, pretium enim quis, faucibus dui. Suspendisse consequat, leo a imperdiet
+              sollicitudin, odio purus faucibus tortor, id venenatis risus neque vel lacus. Pellentesque quis eros ac mi
+              auctor porta. Proin vitae sagittis diam. Nunc nec mi et ante feugiat consequat. Morbi sit amet dictum
+              augue, ac gravida justo. Morbi scelerisque consectetur lacus vitae fermentum.
+            </p>
+            <div>Price: $35</div>
+            <button type="button">Add to cart</button>
+          </article>
+          <article className="App-product">
+            <h3>Item name</h3>
+            <p>
+              Curabitur ut tellus commodo, pretium enim quis, faucibus dui. Suspendisse consequat, leo a imperdiet
+              sollicitudin, odio purus faucibus tortor, id venenatis risus neque vel lacus. Pellentesque quis eros ac mi
+              auctor porta. Proin vitae sagittis diam. Nunc nec mi et ante feugiat consequat. Morbi sit amet dictum
+              augue, ac gravida justo. Morbi scelerisque consectetur lacus vitae fermentum.
+            </p>
+            <div>Price: $35</div>
+            <button type="button">Add to cart</button>
+          </article>
+          <article className="App-product">
+            <h3>Item name</h3>
+            <p>
+              Curabitur ut tellus commodo, pretium enim quis, faucibus dui. Suspendisse consequat, leo a imperdiet
+              sollicitudin, odio purus faucibus tortor, id venenatis risus neque vel lacus. Pellentesque quis eros ac mi
+              auctor porta. Proin vitae sagittis diam. Nunc nec mi et ante feugiat consequat. Morbi sit amet dictum
+              augue, ac gravida justo. Morbi scelerisque consectetur lacus vitae fermentum.
+            </p>
+            <div>Price: $35</div>
+            <button type="button">Add to cart</button>
+          </article>
+        </section>
       </div>
     );
   }
