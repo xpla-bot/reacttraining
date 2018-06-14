6588a579cb6938a32318901bf6e3465aebb35927
Stanislav Lesiuk <stanislav.lesiuk@intive.com>
2018-06-14 08:16:33 +0200

    Switch to JSX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import logoSrc from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    const logo = React.createElement('img', {src: logoSrc, className: 'App-logo', alt: 'logo'});
-    const title = React.createElement('h1', {className: 'App-title'}, ['Welcome to React']);
-    const header = React.createElement('header', {className: 'App-header'}, [logo, title]);
-    const paragraph =
-      React.createElement('p', {className: 'App-intro'}, ['To get started, edit src/App.js and save to reload.']);
-    return React.createElement('div', {className: 'App'}, [header, paragraph]);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
   }
 }
 
