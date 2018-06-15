328b20b67371dd87647a614ab75cf72b35074333
Stanislav Lesiuk <stanislav.lesiuk@intive.com>
2018-06-15 09:25:24 +0200

    Use react-router for navigation instead of state

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header = props => (
+const Header = () => (
   <header className="App-header">
     <nav className="App-nav">
-      <li><a href="#" onClick={() => props.onRedirect('store')}>Store</a></li>
-      <li><a href="#" onClick={() => props.onRedirect('news')}>News</a></li>
-      <li><a href="#" onClick={() => props.onRedirect('other')}>Other</a></li>
+      <li><Link to="/store">Store</Link></li>
+      <li><Link to="/news">News</Link></li>
+      <li><Link to="/other">Other</Link></li>
     </nav>
     <div>
       Cart total: $0
